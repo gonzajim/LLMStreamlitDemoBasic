7cd8fb12dff2d54d6dae0fcf999cba2281f6451a
gonzalojm <gjimenez@jimarsoft.com>
2024-03-12 06:24:26 +0100

    embed_pdf

--- embed_pdf.py
+++ embed_pdf.py
@@ -9,13 +9,7 @@ import boto3
 from botocore.exceptions import NoCredentialsError
 
 
-def embed_document(file_id, drive):
-    # Create a session using your AWS credentials
-    s3 = boto3.client('s3')
-
-    # The name of the bucket
-    bucket_name = os.getenv('S3_BUCKET_NAME')
-
+def embed_document(file_id, drive, s3, bucket_name, collection):
     try:
         s3.download_file(bucket_name, file_id, 'temp.pdf')
     except NoCredentialsError:
@@ -36,40 +30,33 @@ def embed_document(file_id, drive):
     source_chunks = text_splitter.split_documents(source_pages)
     search_index = FAISS.from_documents(source_chunks, embedding_func)
 
-    # Connect to MongoDB Atlas
-    username = os.getenv('MONGODB_USERNAME')
-    password = os.getenv('MONGODB_PASSWORD')
-
-    client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    db = client['mydatabase']
-    collection = db['mycollection']
-
     # Convert the search index to bytes and store it in MongoDB
     index_bytes = pickle.dumps(search_index)
     collection.insert_one({'file_name': file_id, 'index': Binary(index_bytes)})
 
-
-def embed_all_pdf_docs():
+def embed_all_docs():
     # Create a session using your AWS credentials
     s3 = boto3.client('s3')
 
     # The name of the bucket
     bucket_name = os.getenv('S3_BUCKET_NAME')
 
+    # Create a MongoDB client
+    mongo_client = MongoClient(os.getenv('MONGODB_URI'))
+    db = mongo_client[os.getenv('MONGODB_DB_NAME')]
+    collection = db[os.getenv('MONGODB_COLLECTION_NAME')]
+
     try:
         # List all the objects in the bucket
         objects = s3.list_objects(Bucket=bucket_name)['Contents']
 
-        # Filter the list to only include PDF files
-        pdf_files = [obj for obj in objects if obj['Key'].endswith('.pdf')]
-
-        if pdf_files:
-            for pdf_file in pdf_files:
-                print(f"Embedding {pdf_file['Key']}...")
-                embed_document(file_id=pdf_file['Key'], drive=s3)
+        if objects:
+            for obj in objects:
+                print(f"Embedding {obj['Key']}...")
+                embed_document(file_id=obj['Key'], drive=s3, s3=s3, bucket_name=bucket_name, collection=collection)
                 print("Done!")
         else:
-            raise Exception("No PDF files found in the directory.")
+            raise Exception("No files found in the directory.")
     except NoCredentialsError:
         print("No AWS credentials were found.")
 
