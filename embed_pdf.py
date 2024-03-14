8617de7466895380602b8010bb564cd1c66f41e4
gonzalojm <gjimenez@jimarsoft.com>
2024-03-14 08:21:27 +0100

    refactor

--- embed_pdf.py
+++ embed_pdf.py
@@ -4,8 +4,13 @@ from bson.binary import Binary
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 import gridfs
-from llm_helper import embed_and_store_document
 from langchain_community.document_loaders.pdf import PagedPDFSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+import tempfile
 
 def embed_document(file, filename):
     try:
@@ -38,4 +43,27 @@ def embed_document(file, filename):
         print(f"Unsupported file type: {file_type}")
         return
 
-    embed_and_store_document(source_pages, filename, collection)
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small", api_key=OPENAI_API_KEY)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100,
+        length_function=len,
+        is_separator_regex=False,
+        separators=["\n\n", "\n", " ", ""],
+    )
+    source_chunks = text_splitter.split_documents(source_pages)
+    search_index = FAISS.from_documents(source_chunks, embeddings)
+
+    try:
+        # Convert the search index to bytes
+        index_bytes = pickle.dumps(search_index)
+    except (pickle.PicklingError, AttributeError) as e:
+        print(f"Error serializing search index: {e}")
+        return
+
+    try:
+        # Store it in MongoDB
+        collection.insert_one({'file_name': filename, 'index': Binary(index_bytes)})
+    except PyMongoError as e:
+        print(f"Error inserting into MongoDB: {e}")
